0942382fb82e384962258e8578c6d072bbe08ca8
PEDRO DARTORA BESBATI <pedro_besbati@estudante.sesisenai.org.br>
2026-06-17 14:13:55 -0300

    V2

--- config.php
+++ config.php
@@ -4,7 +4,6 @@
         $pdo = new PDO("mysql: host=localhost; dbname=celulares", "root", "senai");
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        echo "deu certo";
     }catch(PDOException $e){
         echo "algo deu de errado".$e->getMessage();
     }
